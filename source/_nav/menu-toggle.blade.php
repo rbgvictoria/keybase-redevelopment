c30b6e8df50a38e9c6215e9f0a4d2a71b1c66197
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-07-07 08:27:28 +0000

    change colors of nav. menu button

--- source/_nav/menu-toggle.blade.php
+++ source/_nav/menu-toggle.blade.php
@@ -1,9 +1,9 @@
-<button class="flex justify-center items-center bg-blue-500 border border-blue-500 h-10 mr-4 px-5 rounded-full lg:hidden focus:outline-none"
+<button class="flex justify-center items-center bg-gray-50 border bg-gray-50 h-10 mr-4 px-5 rounded-full lg:hidden focus:outline-none"
     onclick="navMenu.toggle()"
     aria-label="nav menu"
 >
     <svg id="js-nav-menu-show" xmlns="http://www.w3.org/2000/svg"
-        class="fill-current text-white h-9 w-4" viewBox="0 0 32 32"
+        class="fill-current text-sky-700 h-9 w-4" viewBox="0 0 32 32"
     >
         <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/>
     </svg>
