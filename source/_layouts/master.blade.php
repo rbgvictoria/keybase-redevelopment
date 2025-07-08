534799d7f6c9e618cb8b7d9ca1fd840417f565d8
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2025-07-08 17:19:20 +0000

    update source/_layouts/master.blade.php
    - minor edit to try and force update of site logo URL

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -46,7 +46,6 @@
                 <div class="flex items-center">
                     <a href="{{ $page->siteUrl }}" title="{{ $page->siteName }} home" class="inline-flex items-center">
                         <img class="h-8 md:h-10 mr-3" src="{{ $page->siteUrl }}/assets/img/keybase-logo-80.png" alt="{{ $page->siteName }} logo" />
-
                         <h1 class="text-lg md:text-2xl text-white font-semibold hover:text-gray-200 my-0 pr-4">{{ $page->siteName }}</h1>
                     </a>
                 </div>
